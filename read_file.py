af0e3e42df9e835cb5ac4948020db29b1d41258d
Evie <44837425+evie302@users.noreply.github.com>
2020-07-05 09:08:23 +0100

    Add files via upload

---
+++ read_file.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+pattern = "\.js"
+array = []
+
+file = open("access_log.txt", "rt")
+for line in file:
+    if re.search(pattern, line):
+        extract = line.split("/")[4]
+        extract2 = extract.split(" ")[0]
+        
+        if len(array) < 0:
+            array.append(extract2)
+
+        if extract2 not in array:
+            array.append(extract2)
+
+        #print(extract2)
+
+for a in array:
+    print(a)
